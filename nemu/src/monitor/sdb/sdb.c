612b4526a97f8934006022d8c0acf3b24972eb6c
tracer-ysyx <tracer@ysyx.org>
2026-03-01 20:39:21 +0800

    > compile NEMU ysyx_22040000 李智恒 Linux ecall73-laptop 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  20:39:21 up 43 min,  1 user,  load average: 0.26, 0.24, 0.18

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -54,6 +54,15 @@ static int cmd_q(char *args) {
   return -1;
 }
 
+static int cmd_si(char *args) {
+  int n = 1;
+  if (args != NULL) {
+    n = strtol(args, NULL, 0);
+  }
+  cpu_exec(n);
+  return 0;
+}
+
 static int cmd_help(char *args);
 
 static struct {
@@ -64,6 +73,7 @@ static struct {
   { "help", "Display information about all supported commands", cmd_help },
   { "c", "Continue the execution of the program", cmd_c },
   { "q", "Exit NEMU", cmd_q },
+  { "si [N]", "Pause after executing N instructions", cmd_si },
 
   /* TODO: Add more commands */
 
