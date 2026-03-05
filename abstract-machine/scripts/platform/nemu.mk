4687f90f786fc287c4242576c34daab26370a2db
ecall73 <openweskl@gmail.com>
2026-03-06 02:09:57 +0800

    PA2.2: Programs, Runtime, and the Abstract Machine

--- abstract-machine/scripts/platform/nemu.mk
+++ abstract-machine/scripts/platform/nemu.mk
@@ -13,6 +13,10 @@ LDSCRIPTS += $(AM_HOME)/scripts/linker.ld
 LDFLAGS   += --defsym=_pmem_start=0x80000000 --defsym=_entry_offset=0x0
 LDFLAGS   += --gc-sections -e _start
 NEMUFLAGS += -l $(shell dirname $(IMAGE).elf)/nemu-log.txt
+DEBUG ?= 0
+ifeq ($(filter 1 y yes true,$(DEBUG)),)
+NEMUFLAGS += -b
+endif
 
 MAINARGS_MAX_LEN = 64
 MAINARGS_PLACEHOLDER = the_insert-arg_rule_in_Makefile_will_insert_mainargs_here
