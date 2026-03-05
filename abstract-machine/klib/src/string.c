483c69902a1630dc27388bef63f9cc1919b55053
tracer-ysyx <tracer@ysyx.org>
2026-03-06 01:30:30 +0800

    > compile NEMU ysyx_22040000 李智恒 Linux ecall73-laptop 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  01:30:30 up  5:23,  1 user,  load average: 0.24, 0.22, 0.25

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -4,6 +4,11 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
+#ifdef __GNUC__
+typedef __attribute__((__may_alias__)) size_t WT;
+#define WS (sizeof(WT))
+#endif
+
 size_t strlen(const char *s) {
   const char *a = s;
   for (; *s; s++);
@@ -128,15 +133,39 @@ void *memset(void *dest, int c, size_t n)
 	return dest;
 }
 
-void *memmove(void *dst, const void *src, size_t n) {
-  uint8_t *d = dst;
-  const uint8_t *s = src;
-  if (d < s) {
-    for (; n; n--, d++, s++) *d = *s;
-  } else {
-    for (d += n, s += n; n; n--, d--, s--) *d = *s;
-  }
-  return dst;
+void *memmove(void *dest, const void *src, size_t n)
+{
+	char *d = dest;
+	const char *s = src;
+
+	if (d==s) return d;
+	if ((uintptr_t)s-(uintptr_t)d-n <= -2*n) return memcpy(d, s, n);
+
+	if (d<s) {
+#ifdef __GNUC__
+		if ((uintptr_t)s % WS == (uintptr_t)d % WS) {
+			while ((uintptr_t)d % WS) {
+				if (!n--) return dest;
+				*d++ = *s++;
+			}
+			for (; n>=WS; n-=WS, d+=WS, s+=WS) *(WT *)d = *(WT *)s;
+		}
+#endif
+		for (; n; n--) *d++ = *s++;
+	} else {
+#ifdef __GNUC__
+		if ((uintptr_t)s % WS == (uintptr_t)d % WS) {
+			while ((uintptr_t)(d+n) % WS) {
+				if (!n--) return dest;
+				d[n] = s[n];
+			}
+			while (n>=WS) n-=WS, *(WT *)(d+n) = *(WT *)(s+n);
+		}
+#endif
+		while (n) n--, d[n] = s[n];
+	}
+
+	return dest;
 }
 
 void *memcpy(void *restrict dest, const void *restrict src, size_t n)
