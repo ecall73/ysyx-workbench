89f2606c955a08a27e20b3ef6eff949d01212c04
tracer-ysyx <tracer@ysyx.org>
2026-03-14 16:08:00 +0800

    > compile NEMU ysyx_26030082 李智恒 Linux ecall73-laptop 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  16:08:00 up  1:30,  1 user,  load average: 0.53, 0.57, 0.38

---
+++ paddr.d
@@ -0,0 +1,11 @@
+paddr.o: /home/ecall73/ysyx-workbench/nemu/src/memory/paddr.c \
+ /home/ecall73/ysyx-workbench/nemu/include/memory/host.h \
+ /home/ecall73/ysyx-workbench/nemu/include/common.h \
+ /home/ecall73/ysyx-workbench/nemu/include/generated/autoconf.h \
+ /home/ecall73/ysyx-workbench/nemu/include/macro.h \
+ /home/ecall73/ysyx-workbench/nemu/include/debug.h \
+ /home/ecall73/ysyx-workbench/nemu/include/utils.h \
+ /home/ecall73/ysyx-workbench/nemu/include/memory/paddr.h \
+ /home/ecall73/ysyx-workbench/nemu/include/device/mmio.h \
+ /home/ecall73/ysyx-workbench/nemu/include/isa.h \
+ /home/ecall73/ysyx-workbench/nemu/src/isa/riscv32/include/isa-def.h
