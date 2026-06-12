ed6c0f33f3336c08cee9a06a6e6a765194bedddc
tracer-ysyx <tracer@ysyx.org>
2026-06-13 02:05:53 +0800

    > sim RTL ysyx_26030082 李智恒 Linux ecall73-laptop 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  02:05:53 up  4:22,  1 user,  load average: 0.68, 0.35, 0.24

--- npc/vsrc/core/RF.v
+++ npc/vsrc/core/RF.v
@@ -1,22 +1,24 @@
 module ysyx_26030082_RF(
     input  wire        clock,
     input  wire        reset,
-    // Write rd
     input  wire        wen,
     input  wire [ 4:0] waddr,
     input  wire [31:0] wdata,
-    // Read rs1 rs2
     input  wire [ 4:0] rR1,
     input  wire [ 4:0] rR2,
-
     output reg  [31:0] rR1_data,
     output reg  [31:0] rR2_data
 );
 
     reg [31:0] reg_bank [1:15];
+    integer i;
 
     always @(posedge clock) begin
-        if (wen & (waddr != 5'd0) & ~waddr[4]) begin
+        if (reset) begin
+            for (i = 1; i < 16; i = i + 1) begin
+                reg_bank[i] <= 32'b0;
+            end
+        end else if (wen && (waddr != 5'd0) && !waddr[4]) begin
             reg_bank[waddr[3:0]] <= wdata;
         end
     end
