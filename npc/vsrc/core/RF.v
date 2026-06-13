0ab8986fdffc1854d559c375d3b8180b08d28aaa
tracer-ysyx <tracer@ysyx.org>
2026-06-14 03:53:37 +0800

    > sim RTL ysyx_26030082 李智恒 Linux ecall73-laptop 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  03:53:37 up  2:56,  1 user,  load average: 0.47, 0.25, 0.22

--- npc/vsrc/core/RF.v
+++ npc/vsrc/core/RF.v
@@ -14,12 +14,30 @@ module ysyx_26030082_RF(
 );
 
     reg [31:0] reg_bank [1:15];
+    wire [15:1] write_en;
+    wire [15:1] reg_clock;
 
-    always @(posedge clock) begin
-        if (wen & (waddr != 5'd0) & ~waddr[4]) begin
-            reg_bank[waddr[3:0]] <= wdata;
+    genvar i;
+    generate
+        for (i = 1; i < 16; i = i + 1) begin : gen_reg_write
+            assign write_en[i] = wen && (waddr[3:0] == i[3:0]) && !waddr[4];
+`ifdef SYNTHESIS
+            CLKGATE_X1 u_reg_clock_gate (
+                .CK  (clock),
+                .E   (write_en[i]),
+                .GCK (reg_clock[i])
+            );
+`else
+            assign reg_clock[i] = clock;
+`endif
+            always @(posedge reg_clock[i]) begin
+                if (write_en[i]) begin
+                    reg_bank[i] <= wdata;
+                end
+            end
         end
-    end
+    endgenerate
+
 
     always @(*) begin
         rR1_data = (rR1 == 5'd0 || rR1[4]) ? 32'b0 : reg_bank[rR1[3:0]];
