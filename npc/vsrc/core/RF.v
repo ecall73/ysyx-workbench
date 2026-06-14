a51141e87f59da82d07ec053323fd7bb88e50789
tracer-ysyx <tracer@ysyx.org>
2026-06-14 14:42:04 +0800

    > sim RTL ysyx_26030082 李智恒 Linux ecall73-laptop 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  14:42:04 up 13:01,  1 user,  load average: 0.22, 0.08, 0.08

--- npc/vsrc/core/RF.v
+++ npc/vsrc/core/RF.v
@@ -22,8 +22,43 @@ module ysyx_26030082_RF(
     end
 
     always @(*) begin
-        rR1_data = (rR1 == 5'd0 || rR1[4]) ? 32'b0 : reg_bank[rR1[3:0]];
-        rR2_data = (rR2 == 5'd0 || rR2[4]) ? 32'b0 : reg_bank[rR2[3:0]];
+        case (rR1)
+            5'd1: rR1_data = reg_bank[4'd1];
+            5'd2: rR1_data = reg_bank[4'd2];
+            5'd3: rR1_data = reg_bank[4'd3];
+            5'd4: rR1_data = reg_bank[4'd4];
+            5'd5: rR1_data = reg_bank[4'd5];
+            5'd6: rR1_data = reg_bank[4'd6];
+            5'd7: rR1_data = reg_bank[4'd7];
+            5'd8: rR1_data = reg_bank[4'd8];
+            5'd9: rR1_data = reg_bank[4'd9];
+            5'd10: rR1_data = reg_bank[4'd10];
+            5'd11: rR1_data = reg_bank[4'd11];
+            5'd12: rR1_data = reg_bank[4'd12];
+            5'd13: rR1_data = reg_bank[4'd13];
+            5'd14: rR1_data = reg_bank[4'd14];
+            5'd15: rR1_data = reg_bank[4'd15];
+            default: rR1_data = 32'b0;
+        endcase
+
+        case (rR2)
+            5'd1: rR2_data = reg_bank[4'd1];
+            5'd2: rR2_data = reg_bank[4'd2];
+            5'd3: rR2_data = reg_bank[4'd3];
+            5'd4: rR2_data = reg_bank[4'd4];
+            5'd5: rR2_data = reg_bank[4'd5];
+            5'd6: rR2_data = reg_bank[4'd6];
+            5'd7: rR2_data = reg_bank[4'd7];
+            5'd8: rR2_data = reg_bank[4'd8];
+            5'd9: rR2_data = reg_bank[4'd9];
+            5'd10: rR2_data = reg_bank[4'd10];
+            5'd11: rR2_data = reg_bank[4'd11];
+            5'd12: rR2_data = reg_bank[4'd12];
+            5'd13: rR2_data = reg_bank[4'd13];
+            5'd14: rR2_data = reg_bank[4'd14];
+            5'd15: rR2_data = reg_bank[4'd15];
+            default: rR2_data = 32'b0;
+        endcase
     end
 
 endmodule
