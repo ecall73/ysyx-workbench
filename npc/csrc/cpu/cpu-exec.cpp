885ed8d022150bd686319664ec229268e3be5a73
tracer-ysyx <tracer@ysyx.org>
2026-06-20 21:15:07 +0800

    > sim RTL ysyx_26030082 李智恒 Linux ecall73-laptop 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  21:15:07 up  4:25,  1 user,  load average: 0.18, 0.15, 0.16

--- npc/csrc/cpu/cpu-exec.cpp
+++ npc/csrc/cpu/cpu-exec.cpp
@@ -207,16 +207,16 @@ static void statistic() {
         "redirect / ctrl", "-", 100.0 * ratio(g_pmu.redirect, control_flow));
     PmuLog("+------------------+-------------+-----------+");
 
-    PmuLog("=== PMU commit table (type/count/ratio) ===");
-    PmuLog("+------------------+-----------+-----------+");
-    PmuLog("| %-16s | %9s | %9s |", "type", "count", "ratio");
-    PmuLog("+------------------+-----------+-----------+");
+    PmuLog("==== PMU commit table (type/count/ratio) ====");
+    PmuLog("+------------------+-------------+-----------+");
+    PmuLog("| %-16s | %11s | %9s |", "type", "count", "ratio");
+    PmuLog("+------------------+-------------+-----------+");
     for (int i = 0; i < PMU_CLASS_COUNT; i++) {
         double retire_mix = 100.0 * ratio(g_ret_class_cnt[i], g_nr_guest_inst);
-        PmuLog("| %-16s | %9" PRIu64 " | %8.4f%% |",
+        PmuLog("| %-16s | %11" PRIu64 " | %8.4f%% |",
             kPmuClassName[i], g_ret_class_cnt[i], retire_mix);
     }
-    PmuLog("+------------------+-----------+-----------+");
+    PmuLog("+------------------+-------------+-----------+");
 }
 
 void cpu_exec(uint64_t n) {
