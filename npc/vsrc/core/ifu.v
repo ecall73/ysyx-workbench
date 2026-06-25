9d12e5ef6ae955caa2f3ba1cd34d168e3f923c0c
tracer-ysyx <tracer@ysyx.org>
2026-06-25 21:31:39 +0800

    > sim RTL ysyx_26030082 李智恒 Linux ecall73-laptop 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  21:31:39 up  6:21,  1 user,  load average: 0.81, 0.82, 0.59

--- npc/vsrc/core/ifu.v
+++ npc/vsrc/core/ifu.v
@@ -74,6 +74,7 @@ module ysyx_26030082_ifu #(
     assign ifu_master_rready = (state == S_MISS_R);
     wire ar_fire = ifu_master_arvalid && ifu_master_arready;
     wire r_fire = ifu_master_rvalid && ifu_master_rready;
+    wire refill_drop = drop_refill || flush;
 
     always @(posedge clock) begin
         if (reset) begin
@@ -82,7 +83,7 @@ module ysyx_26030082_ifu #(
             valid_array <= {LINE_COUNT{1'b0}};
         end else if (ar_fire) begin
             valid_array[lookup_index] <= 1'b0;
-        end else if (r_fire && ifu_master_rlast && !drop_refill && !flush) begin
+        end else if ((state == S_MISS_R) && r_fire && ifu_master_rlast && !refill_drop) begin
             valid_array[lookup_index] <= 1'b1;
         end
     end
@@ -115,12 +116,12 @@ module ysyx_26030082_ifu #(
                     end
 
                     if (r_fire) begin
-                        if (!drop_refill && !flush) begin
+                        if (!refill_drop) begin
                             data_array[lookup_index][{refill_word_idx, 5'b0} +: 32] <= ifu_master_rdata;
                         end
 
                         if (ifu_master_rlast) begin
-                            if (!drop_refill && !flush) begin
+                            if (!refill_drop) begin
                                 tag_array[lookup_index] <= lookup_tag;
                             end
                             state <= S_LOOKUP;
