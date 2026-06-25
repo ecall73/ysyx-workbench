82203e73542a81699fffa78d1c8965745499e12c
tracer-ysyx <tracer@ysyx.org>
2026-06-25 20:19:39 +0800

    > sim RTL ysyx_26030082 李智恒 Linux ecall73-laptop 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  20:19:39 up  5:11,  1 user,  load average: 0.40, 0.43, 0.25

--- npc/vsrc/core/exu.v
+++ npc/vsrc/core/exu.v
@@ -392,7 +392,12 @@ module ysyx_26030082_exu (
         end else begin
             case (wr_state)
                 W_IDLE: begin
-                    wr_state <= {aw_fire, w_fire};
+                    case ({aw_fire, w_fire})
+                        2'b11: wr_state <= W_WAIT_B;
+                        2'b10: wr_state <= W_WAIT_W;
+                        2'b01: wr_state <= W_WAIT_AW;
+                        default:;
+                    endcase
                 end
                 W_WAIT_AW: if (aw_fire) wr_state <= W_WAIT_B;
                 W_WAIT_W:  if (w_fire)  wr_state <= W_WAIT_B;
