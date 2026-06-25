280401eec6b5b75b93bc0d833d50874bccc181ba
tracer-ysyx <tracer@ysyx.org>
2026-06-26 07:55:59 +0800

    > sim RTL ysyx_26030082 李智恒 Linux ecall73-laptop 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  07:55:59 up 16:16,  1 user,  load average: 0.39, 0.46, 0.53

--- npc/vsrc/core/ifu.v
+++ npc/vsrc/core/ifu.v
@@ -36,6 +36,7 @@ module ysyx_26030082_ifu #(
     localparam integer INDEX_W         = $clog2(LINE_COUNT);
     localparam integer OFFSET_W        = WORD_OFF_W + LINE_ADDR_OFF_W;
     localparam integer TAG_W           = 32 - INDEX_W - OFFSET_W;
+    localparam integer CACHE_WORDS     = LINE_WORDS * LINE_COUNT;
 
     localparam [1:0] S_LOOKUP  = 2'd0;
     localparam [1:0] S_MISS_AR = 2'd1;
@@ -44,8 +45,7 @@ module ysyx_26030082_ifu #(
 
     reg [1:0] state;
 
-    localparam integer LINE_BITS = LINE_WORDS * 32;
-    reg [LINE_BITS-1:0] data_array [0:LINE_COUNT-1];
+    reg [31:0] data_array [0:CACHE_WORDS-1];
     reg [TAG_W-1:0] tag_array [0:LINE_COUNT-1];
     reg [LINE_COUNT-1:0] valid_array;
 
@@ -55,7 +55,8 @@ module ysyx_26030082_ifu #(
         if_pc[WORD_OFF_W + LINE_WORD_OFF_W - 1 : WORD_OFF_W];
     wire [INDEX_W-1:0] lookup_index = if_pc[OFFSET_W + INDEX_W - 1 : OFFSET_W];
     wire [TAG_W-1:0] lookup_tag = if_pc[31 : OFFSET_W + INDEX_W];
-    wire [LINE_BITS-1:0] lookup_line = data_array[lookup_index];
+    wire [LINE_WORD_OFF_W + INDEX_W - 1:0] lookup_word_index = {lookup_index, lookup_word_offset};
+    wire [LINE_WORD_OFF_W + INDEX_W - 1:0] refill_word_index = {lookup_index, refill_word_idx};
 
     wire cache_hit = valid_array[lookup_index] && (tag_array[lookup_index] == lookup_tag);
     wire cache_miss = (state == S_LOOKUP) && !cache_hit;
@@ -63,7 +64,7 @@ module ysyx_26030082_ifu #(
     wire flush = ex_out_valid && ex_redirect;
     wire invalidate = ex_out_valid && ex_fence_i;
     assign if_out_valid = (state == S_LOOKUP) && cache_hit;
-    assign if_inst = lookup_line[{lookup_word_offset, 5'b0} +: 32];
+    assign if_inst = data_array[lookup_word_index];
 
     wire req_fire = if_out_valid && if_out_ready;
 
@@ -121,7 +122,7 @@ module ysyx_26030082_ifu #(
                     if (flush) begin
                         state <= (r_fire && ifu_master_rlast) ? S_LOOKUP : S_DROP_R;
                     end else if (r_fire) begin
-                        data_array[lookup_index][{refill_word_idx, 5'b0} +: 32] <= ifu_master_rdata;
+                        data_array[refill_word_index] <= ifu_master_rdata;
                         if (ifu_master_rlast) begin
                             state <= S_LOOKUP;
                         end else begin
