b021c63df073f143fba0bd90a5f41ca0bbd5ead0
tracer-ysyx <tracer@ysyx.org>
2026-06-29 23:30:57 +0800

    > compile NEMU ysyx_26030082 李智恒 Linux ecall73-laptop 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  23:30:57 up 1 day,  8:05,  1 user,  load average: 0.22, 0.21, 0.20

--- abstract-machine/klib/src/newlib.c
+++ abstract-machine/klib/src/newlib.c
@@ -7,7 +7,31 @@
 
 #if defined(__KLIB_USE_NEWLIB__) || defined(__KLIB_USE_PICOLIBC__)
 
+#ifdef __KLIB_USE_PICOLIBC__
+#include <stdio.h>
+#endif
+
 static char *brk = NULL;
+
+#ifdef __KLIB_USE_PICOLIBC__
+static int am_stdio_put(char ch, FILE *stream) {
+  (void)stream;
+  putch(ch);
+  return 0;
+}
+
+static int am_stdio_get(FILE *stream) {
+  (void)stream;
+  return _FDEV_EOF;
+}
+
+static FILE am_stdin = FDEV_SETUP_STREAM(NULL, am_stdio_get, NULL, _FDEV_SETUP_READ);
+static FILE am_stdout = FDEV_SETUP_STREAM(am_stdio_put, NULL, NULL, _FDEV_SETUP_WRITE);
+
+FILE *const stdin = &am_stdin;
+FILE *const stdout = &am_stdout;
+FILE *const stderr = &am_stdout;
+#endif
 
 static void *am_sbrk(ptrdiff_t incr) {
   if (brk == NULL) brk = heap.start;
