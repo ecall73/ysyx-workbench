25c60292eec0bafab4bb6a15d468a983d19903bf
tracer-ysyx <tracer@ysyx.org>
2026-06-29 21:50:05 +0800

    > compile NEMU ysyx_26030082 李智恒 Linux ecall73-laptop 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  21:50:05 up 1 day,  6:24,  1 user,  load average: 0.19, 0.13, 0.10

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -93,12 +93,6 @@ static inline uint32_t csr_addr(const Decode *s) {
   return BITS(s->isa.inst, 31, 20);
 }
 
-#if CONFIG_ETRACE
-static inline void etrace_log_mret(vaddr_t target) {
-  etrace_write("etrace: mret -> " FMT_WORD " mstatus=" FMT_WORD "\n", target, cpu.mstatus);
-}
-#endif
-
 static int decode_exec(Decode *s) {
   s->dnpc = s->snpc;
 
@@ -240,7 +234,7 @@ static int decode_exec(Decode *s) {
       mstatus &= ~MSTATUS_MPP;                                        // MPP <- U(0)
       cpu.mstatus = mstatus;
       s->dnpc = cpu.mepc;
-      IFONE(CONFIG_ETRACE, etrace_log_mret(s->dnpc));
+      IFONE(CONFIG_ETRACE, etrace_write("etrace: mret -> " FMT_WORD " mstatus=" FMT_WORD "\n", s->dnpc, cpu.mstatus));
   );
   INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak , N, NEMUTRAP(s->pc, R(10))); // R(10) is $a0
 
