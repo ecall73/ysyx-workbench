a8cae36ac8904297edc854005c9213cf2f055d01
tracer-ysyx <tracer@ysyx.org>
2026-06-30 00:39:45 +0800

    > compile NEMU ysyx_26030082 李智恒 Linux ecall73-laptop 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  00:39:45 up 1 day,  9:13,  1 user,  load average: 0.09, 0.13, 0.16

--- abstract-machine/scripts/isa/riscv.mk
+++ abstract-machine/scripts/isa/riscv.mk
@@ -6,3 +6,11 @@ LDFLAGS       += -melf64lriscv
 
 # overwrite ARCH_H defined in $(AM_HOME)/Makefile
 ARCH_H := arch/riscv.h
+
+ifeq ($(PICOLIBC),1)
+KLIB_PICOLIBC_HOME ?= /usr/lib/picolibc/riscv64-unknown-elf
+KLIB_LIBC ?= $(KLIB_PICOLIBC_HOME)/lib/$(KLIB_PICOLIBC_MULTILIB)/libc.a
+KLIB_LIBGCC ?= /usr/lib/gcc/riscv64-unknown-elf/10.2.0/$(KLIB_PICOLIBC_MULTILIB)/libgcc.a
+CFLAGS += -D__KLIB_USE_PICOLIBC__ -isystem $(KLIB_PICOLIBC_HOME)/include
+LINKAGE += $(KLIB_LIBC) $(KLIB_LIBGCC)
+endif
