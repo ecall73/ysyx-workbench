35cdbb45a05f44d663a9970df5e188b050b963aa
tracer-ysyx <tracer@ysyx.org>
2026-06-30 15:18:03 +0800

    > compile NEMU ysyx_26030082 李智恒 Linux ecall73-laptop 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  15:18:03 up 1 day, 23:26,  1 user,  load average: 0.36, 0.20, 0.11

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -18,12 +18,7 @@
 #include <difftest-def.h>
 #include <device/mmio.h>
 #include <isa.h>
-
-void ysyxsoc_paddr_init(void);
-bool ysyxsoc_paddr_read(paddr_t addr, int len, word_t *data);
-bool ysyxsoc_paddr_write(paddr_t addr, int len, word_t data);
-void ysyxsoc_paddr_log_ranges(void);
-void ysyxsoc_paddr_out_of_bound(paddr_t addr);
+#include <string.h>
 
 #if   defined(CONFIG_PMEM_MALLOC)
 static uint8_t *pmem = NULL;
@@ -33,6 +28,9 @@ static uint8_t pmem[CONFIG_MSIZE] PG_ALIGN = {};
 
 static bool use_ysyxsoc_paddr = false;
 
+/*
+ * Native NEMU memory map
+ */
 uint8_t* guest_to_host(paddr_t paddr) { return pmem + paddr - CONFIG_MBASE; }
 paddr_t host_to_guest(uint8_t *haddr) { return haddr - pmem + CONFIG_MBASE; }
 
@@ -47,6 +45,88 @@ static void pmem_write(paddr_t addr, int len, word_t data) {
 static void out_of_bound(paddr_t addr) {
   panic("address = " FMT_PADDR " is out of bound of pmem [" FMT_PADDR ", " FMT_PADDR "] at pc = " FMT_WORD,
       addr, PMEM_LEFT, PMEM_RIGHT, cpu.pc);
+}
+
+/*
+ * ysyxSoC memory map for NEMU-as-ref
+ */
+#define FLASH_BASE 0x30000000u
+#define FLASH_SIZE (16u * 1024u * 1024u)
+#define FLASH_RIGHT (FLASH_BASE + FLASH_SIZE - 1)
+#define SRAM_BASE 0x0f000000u
+#define SRAM_SIZE 0x2000u
+#define SRAM_RIGHT (SRAM_BASE + SRAM_SIZE - 1)
+#define SDRAM_BASE 0xa0000000u
+#define SDRAM_SIZE (32u * 1024u * 1024u)
+#define SDRAM_RIGHT (SDRAM_BASE + SDRAM_SIZE - 1)
+
+static uint8_t flash[FLASH_SIZE] = {};
+static uint8_t sram[SRAM_SIZE] = {};
+static uint8_t sdram[SDRAM_SIZE] = {};
+
+static bool in_region(paddr_t addr, int len, paddr_t base, uint32_t size) {
+  if (len <= 0) return false;
+  if (addr < base) return false;
+  uint64_t off = (uint64_t)(addr - base);
+  return off + (uint64_t)len <= (uint64_t)size;
+}
+
+static void ysyxsoc_paddr_init(void) {
+  memset(flash, 0, sizeof(flash));
+  memset(sram, 0, sizeof(sram));
+  memset(sdram, 0, sizeof(sdram));
+}
+
+static bool ysyxsoc_paddr_read(paddr_t addr, int len, word_t *data) {
+  if (in_region(addr, len, FLASH_BASE, FLASH_SIZE)) {
+    *data = host_read(flash + (addr - FLASH_BASE), len);
+    return true;
+  }
+  if (in_region(addr, len, SRAM_BASE, SRAM_SIZE)) {
+    *data = host_read(sram + (addr - SRAM_BASE), len);
+    return true;
+  }
+  if (in_region(addr, len, SDRAM_BASE, SDRAM_SIZE)) {
+    *data = host_read(sdram + (addr - SDRAM_BASE), len);
+    return true;
+  }
+  IFDEF(CONFIG_DEVICE, {
+    *data = mmio_read(addr, len);
+    return true;
+  });
+  return false;
+}
+
+static bool ysyxsoc_paddr_write(paddr_t addr, int len, word_t data) {
+  if (in_region(addr, len, FLASH_BASE, FLASH_SIZE)) {
+    host_write(flash + (addr - FLASH_BASE), len, data);
+    return true;
+  }
+  if (in_region(addr, len, SRAM_BASE, SRAM_SIZE)) {
+    host_write(sram + (addr - SRAM_BASE), len, data);
+    return true;
+  }
+  if (in_region(addr, len, SDRAM_BASE, SDRAM_SIZE)) {
+    host_write(sdram + (addr - SDRAM_BASE), len, data);
+    return true;
+  }
+  IFDEF(CONFIG_DEVICE, {
+    mmio_write(addr, len, data);
+    return true;
+  });
+  return false;
+}
+
+static void ysyxsoc_paddr_log_ranges(void) {
+  Log("FLASH area [0x%08x, 0x%08x]", FLASH_BASE, FLASH_RIGHT);
+  Log("SRAM area [0x%08x, 0x%08x]", SRAM_BASE, SRAM_RIGHT);
+  Log("SDRAM area [0x%08x, 0x%08x]", SDRAM_BASE, SDRAM_RIGHT);
+}
+
+static void ysyxsoc_paddr_out_of_bound(paddr_t addr) {
+  panic("address = " FMT_PADDR " is out of bound for ysyxsoc memory. valid ranges: flash[0x%08x, 0x%08x], "
+      "sram[0x%08x, 0x%08x], sdram[0x%08x, 0x%08x], pc = " FMT_WORD,
+      addr, FLASH_BASE, FLASH_RIGHT, SRAM_BASE, SRAM_RIGHT, SDRAM_BASE, SDRAM_RIGHT, cpu.pc);
 }
 
 __EXPORT void difftest_enable_ysyxsoc_paddr(void) {
