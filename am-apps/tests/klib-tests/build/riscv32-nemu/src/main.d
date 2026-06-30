fe3048eb4fede5e12a9cea1efb5d6c89350df540
tracer-ysyx <tracer@ysyx.org>
2026-06-30 13:12:32 +0800

    > compile NEMU ysyx_26030082 李智恒 Linux ecall73-laptop 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  13:12:32 up 1 day, 21:25,  1 user,  load average: 0.14, 0.22, 0.18

---
+++ am-apps/tests/klib-tests/build/riscv32-nemu/src/main.d
@@ -0,0 +1,6 @@
+/home/ecall73/ysyx-workbench/am-apps/tests/klib-tests/build/riscv32-nemu/src/main.o: \
+ /home/ecall73/ysyx-workbench/am-apps/tests/klib-tests/src/main.c \
+ /home/ecall73/ysyx-workbench/abstract-machine/am/include/am.h \
+ /home/ecall73/ysyx-workbench/abstract-machine/am/include/arch/riscv.h \
+ /home/ecall73/ysyx-workbench/abstract-machine/am/include/amdev.h \
+ /home/ecall73/ysyx-workbench/abstract-machine/klib/include/klib.h
