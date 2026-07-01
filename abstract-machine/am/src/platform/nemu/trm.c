cb62c3edeee583444fab0ca3dd96b5e7021a615c
tracer-ysyx <tracer@ysyx.org>
2026-07-02 03:26:35 +0800

    > compile NEMU ysyx_26030082 李智恒 Linux ecall73-laptop 6.18.33.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun 18 21:54:43 UTC 2026 x86_64 x86_64 x86_64 GNU/Linux  03:26:35 up 16:52,  1 user,  load average: 1.16, 1.17, 1.19

--- abstract-machine/am/src/platform/nemu/trm.c
+++ abstract-machine/am/src/platform/nemu/trm.c
@@ -7,10 +7,6 @@ int main(const char *args);
 Area heap = RANGE(&_heap_start, PMEM_END);
 static const char mainargs[MAINARGS_MAX_LEN] = TOSTRING(MAINARGS_PLACEHOLDER); // defined in CFLAGS
 
-_Static_assert(MAINARGS_MAX_LEN > 0, "MAINARGS_MAX_LEN must be positive");
-_Static_assert(sizeof(TOSTRING(MAINARGS_PLACEHOLDER)) <= MAINARGS_MAX_LEN,
-    "mainargs placeholder exceeds MAINARGS_MAX_LEN");
-
 void putch(char ch) {
   outb(SERIAL_PORT, ch);
 }
--- abstract-machine/am/src/riscv/ysyxsoc/include/npc.h
+++ abstract-machine/am/src/riscv/ysyxsoc/include/npc.h
@@ -69,7 +69,6 @@ typedef uintptr_t PTE;
 
 #define PGSIZE    4096
 
-_Static_assert((SERIAL_PORT & 0x0) == 0, "SERIAL_PORT alignment check");
 _Static_assert((CLINT_MTIME & 0x3) == 0, "CLINT_MTIME must be word aligned");
 _Static_assert((CLINT_MTIMEH & 0x3) == 0, "CLINT_MTIMEH must be word aligned");
 _Static_assert((RTC_ADDR & 0x3) == 0, "RTC_ADDR must be word aligned");
