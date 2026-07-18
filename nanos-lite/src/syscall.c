2d717e6ac6b3b1b7f730137e0bc46bc95cb7524d
ecall73 <openweskl@gmail.com>
2026-07-19 02:11:40 +0800

    PA3.2: Run Hello World on Nanos-lite

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -7,17 +7,36 @@ void do_syscall(Context *c) {
   a[2] = c->GPR3;
   a[3] = c->GPR4;
 
+#if STRACE
+  const char *name;
+  switch (a[0]) {
+    case SYS_exit:  name = "exit";  break;
+    case SYS_yield: name = "yield"; break;
+    case SYS_write: name = "write"; break;
+    default:        name = "unknown";
+  }
+  Log("strace: %s(%p, %p, %p)", name, (void *)a[1], (void *)a[2], (void *)a[3]);
+#endif
+
   switch (a[0]) {
     case SYS_exit:
-      Log("strace: exit(%p, %p, %p)", (void *)a[1], (void *)a[2], (void *)a[3]);
       halt((int)a[1]);
       break;
     case SYS_yield:
-      Log("strace: yield(%p, %p, %p)", (void *)a[1], (void *)a[2], (void *)a[3]);
       yield();
       c->GPRx = 0;
-      Log("strace: yield -> %p", (void *)c->GPRx);
+      break;
+    case SYS_write:
+      assert(a[1] == 1 || a[1] == 2);
+      for (size_t i = 0; i < a[3]; i++) {
+        putch(((const char *)a[2])[i]);
+      }
+      c->GPRx = a[3];
       break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
+
+#if STRACE
+  Log("strace: %s -> %p", name, (void *)c->GPRx);
+#endif
 }
