c2d6c6228bf377c353fb8db51b62de2e35c91ff5
ecall73 <openweskl@gmail.com>
2026-07-19 00:00:36 +0800

    PA3.2: Implement loader

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -9,9 +9,27 @@
 # define Elf_Phdr Elf32_Phdr
 #endif
 
+extern size_t ramdisk_read(void *buf, size_t offset, size_t len);
+
 static uintptr_t loader(PCB *pcb, const char *filename) {
-  TODO();
-  return 0;
+  Elf_Ehdr ehdr;
+  Elf_Phdr phdr;
+
+  ramdisk_read(&ehdr, 0, sizeof(ehdr));
+  assert(ehdr.e_phentsize == sizeof(phdr));
+
+  for (int i = 0; i < ehdr.e_phnum; i++) {
+    ramdisk_read(&phdr, ehdr.e_phoff + i * ehdr.e_phentsize, sizeof(phdr));
+    if (phdr.p_type != PT_LOAD) {
+      continue;
+    }
+
+    assert(phdr.p_filesz <= phdr.p_memsz);
+    ramdisk_read((void *)phdr.p_vaddr, phdr.p_offset, phdr.p_filesz);
+    memset((void *)(phdr.p_vaddr + phdr.p_filesz), 0, phdr.p_memsz - phdr.p_filesz);
+  }
+
+  return ehdr.e_entry;
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
