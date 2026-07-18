f94ca370838cff43ec8e78e19118269df7712a83
tracer-ysyx <tracer@ysyx.org>
2026-07-19 01:59:08 +0800

    > compile NEMU ysyx_26030082 李智恒 Linux ecall73-laptop 6.18.33.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun 18 21:54:43 UTC 2026 x86_64 x86_64 x86_64 GNU/Linux  01:59:08 up 10:48,  1 user,  load average: 0.08, 0.11, 0.09

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -9,23 +9,33 @@ void do_syscall(Context *c) {
 
   switch (a[0]) {
     case SYS_exit:
+#if STRACE
       Log("strace: exit(%p, %p, %p)", (void *)a[1], (void *)a[2], (void *)a[3]);
+#endif
       halt((int)a[1]);
       break;
     case SYS_yield:
+#if STRACE
       Log("strace: yield(%p, %p, %p)", (void *)a[1], (void *)a[2], (void *)a[3]);
+#endif
       yield();
       c->GPRx = 0;
+#if STRACE
       Log("strace: yield -> %p", (void *)c->GPRx);
+#endif
       break;
     case SYS_write:
+#if STRACE
       Log("strace: write(%p, %p, %p)", (void *)a[1], (void *)a[2], (void *)a[3]);
+#endif
       assert(a[1] == 1 || a[1] == 2);
       for (size_t i = 0; i < a[3]; i++) {
         putch(((const char *)a[2])[i]);
       }
       c->GPRx = a[3];
+#if STRACE
       Log("strace: write -> %p", (void *)c->GPRx);
+#endif
       break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
