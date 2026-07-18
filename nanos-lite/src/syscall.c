5076f73131a6201528f438b82887444872a15a80
tracer-ysyx <tracer@ysyx.org>
2026-07-19 01:39:31 +0800

    > compile NEMU ysyx_26030082 李智恒 Linux ecall73-laptop 6.18.33.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun 18 21:54:43 UTC 2026 x86_64 x86_64 x86_64 GNU/Linux  01:39:31 up 10:30,  1 user,  load average: 0.16, 0.16, 0.13

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,11 +1,24 @@
 #include <common.h>
 #include "syscall.h"
+
+static const char *syscall_name(uintptr_t no) {
+  switch (no) {
+    case SYS_exit: return "exit";
+    case SYS_yield: return "yield";
+    default: return "unknown";
+  }
+}
+
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
   a[1] = c->GPR2;
   a[2] = c->GPR3;
   a[3] = c->GPR4;
+
+  const char *name = syscall_name(a[0]);
+  Log("strace: %s(%p, %p, %p)", name, (void *)a[1], (void *)a[2],
+      (void *)a[3]);
 
   switch (a[0]) {
     case SYS_exit:
@@ -17,4 +30,6 @@ void do_syscall(Context *c) {
       break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
+
+  Log("strace: %s -> %p", name, (void *)c->GPRx);
 }
