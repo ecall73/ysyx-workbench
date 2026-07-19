85ab34071fed6dd02d7ac4506d99cc76b8484d48
ecall73 <openweskl@gmail.com>
2026-07-20 01:28:58 +0800

    PA3.3: Let the loader use files

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -7,6 +7,7 @@ typedef struct {
   char *name;
   size_t size;
   size_t disk_offset;
+  size_t open_offset;
   ReadFn read;
   WriteFn write;
 } Finfo;
@@ -25,11 +26,84 @@ size_t invalid_write(const void *buf, size_t offset, size_t len) {
 
 /* This is the information about all files in disk. */
 static Finfo file_table[] __attribute__((used)) = {
-  [FD_STDIN]  = {"stdin", 0, 0, invalid_read, invalid_write},
-  [FD_STDOUT] = {"stdout", 0, 0, invalid_read, invalid_write},
-  [FD_STDERR] = {"stderr", 0, 0, invalid_read, invalid_write},
+  [FD_STDIN]  = {"stdin", 0, 0, 0, invalid_read, invalid_write},
+  [FD_STDOUT] = {"stdout", 0, 0, 0, invalid_read, invalid_write},
+  [FD_STDERR] = {"stderr", 0, 0, 0, invalid_read, invalid_write},
 #include "files.h"
 };
+
+extern size_t ramdisk_read(void *buf, size_t offset, size_t len);
+
+#define NR_FILES (sizeof(file_table) / sizeof(file_table[0]))
+
+static Finfo *get_file(int fd) {
+  assert(fd >= 0 && fd < (int)NR_FILES);
+  return &file_table[fd];
+}
+
+int fs_open(const char *pathname, int flags, int mode) {
+  (void)flags;
+  (void)mode;
+
+  for (int i = 0; i < (int)NR_FILES; i ++) {
+    if (strcmp(pathname, file_table[i].name) == 0) {
+      file_table[i].open_offset = 0;
+      return i;
+    }
+  }
+
+  assert(0);
+  return -1;
+}
+
+size_t fs_read(int fd, void *buf, size_t len) {
+  Finfo *file = get_file(fd);
+  if (fd <= FD_STDERR || file->open_offset == file->size) {
+    return 0;
+  }
+
+  if (len > file->size - file->open_offset) {
+    len = file->size - file->open_offset;
+  }
+
+  if (file->read != NULL) {
+    len = file->read(buf, file->open_offset, len);
+  } else {
+    len = ramdisk_read(buf, file->disk_offset + file->open_offset, len);
+  }
+  file->open_offset += len;
+  return len;
+}
+
+size_t fs_lseek(int fd, size_t offset, int whence) {
+  Finfo *file = get_file(fd);
+  size_t new_offset;
+
+  switch (whence) {
+    case SEEK_SET:
+      new_offset = offset;
+      break;
+    case SEEK_CUR:
+      assert(offset <= file->size - file->open_offset);
+      new_offset = file->open_offset + offset;
+      break;
+    case SEEK_END:
+      assert(offset == 0);
+      new_offset = file->size;
+      break;
+    default:
+      assert(0);
+      return 0;
+  }
+
+  assert(new_offset <= file->size);
+  file->open_offset = new_offset;
+  return new_offset;
+}
+
+int fs_close(int fd) {
+  return 0;
+}
 
 void init_fs() {
   // TODO: initialize the size of /dev/fb
