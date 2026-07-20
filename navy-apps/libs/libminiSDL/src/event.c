3723f0d6d5f3cd03a0a3aae2b572e20e53966434
ecall73 <openweskl@gmail.com>
2026-07-21 01:59:33 +0800

    PA3.3: Run NSlider (2)

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -1,5 +1,7 @@
 #include <NDL.h>
 #include <SDL.h>
+#include <assert.h>
+#include <string.h>
 
 #define keyname(k) #k,
 
@@ -17,7 +19,20 @@ int SDL_PollEvent(SDL_Event *ev) {
 }
 
 int SDL_WaitEvent(SDL_Event *event) {
-  return 1;
+  char buf[64];
+  while (!NDL_PollEvent(buf, sizeof(buf)));
+
+  buf[strlen(buf) - 1] = '\0';
+  event->type = buf[1] == 'd' ? SDL_KEYDOWN : SDL_KEYUP;
+  for (int i = 1; i < (int)(sizeof(keyname) / sizeof(keyname[0])); i++) {
+    if (strcmp(buf + 3, keyname[i]) == 0) {
+      event->key.keysym.sym = i;
+      return 1;
+    }
+  }
+
+  assert(0);
+  return 0;
 }
 
 int SDL_PeepEvents(SDL_Event *ev, int numevents, int action, uint32_t mask) {
