8778c8dbb602c9676238e5c620d3fd1c6dc386e4
ecall73 <openweskl@gmail.com>
2026-07-21 02:25:43 +0800

    PA3.3: Run NTerm

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -15,24 +15,26 @@ int SDL_PushEvent(SDL_Event *ev) {
 }
 
 int SDL_PollEvent(SDL_Event *ev) {
-  return 0;
-}
-
-int SDL_WaitEvent(SDL_Event *event) {
+  assert(ev);
   char buf[64];
-  while (!NDL_PollEvent(buf, sizeof(buf)));
+  if (!NDL_PollEvent(buf, sizeof(buf))) return 0;
 
   buf[strlen(buf) - 1] = '\0';
-  event->type = buf[1] == 'd' ? SDL_KEYDOWN : SDL_KEYUP;
+  ev->type = buf[1] == 'd' ? SDL_KEYDOWN : SDL_KEYUP;
   for (int i = 1; i < (int)(sizeof(keyname) / sizeof(keyname[0])); i++) {
     if (strcmp(buf + 3, keyname[i]) == 0) {
-      event->key.keysym.sym = i;
+      ev->key.keysym.sym = i;
       return 1;
     }
   }
 
   assert(0);
   return 0;
+}
+
+int SDL_WaitEvent(SDL_Event *event) {
+  while (!SDL_PollEvent(event));
+  return 1;
 }
 
 int SDL_PeepEvents(SDL_Event *ev, int numevents, int action, uint32_t mask) {
--- navy-apps/libs/libminiSDL/src/timer.c
+++ navy-apps/libs/libminiSDL/src/timer.c
@@ -11,7 +11,14 @@ int SDL_RemoveTimer(SDL_TimerID id) {
 }
 
 uint32_t SDL_GetTicks() {
-  return 0;
+  static uint32_t init_time = 0;
+  static int initialized = 0;
+  uint32_t now = NDL_GetTicks();
+  if (!initialized) {
+    init_time = now;
+    initialized = 1;
+  }
+  return now - init_time;
 }
 
 void SDL_Delay(uint32_t ms) {
