9b989ee369f0423b74290f6475f10bbd6964f978
tracer-ysyx <tracer@ysyx.org>
2026-07-22 03:57:27 +0800

    > compile NEMU ysyx_26030082 李智恒 Linux ecall73-laptop 6.18.33.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun 18 21:54:43 UTC 2026 x86_64 x86_64 x86_64 GNU/Linux  03:57:27 up 12:16,  1 user,  load average: 0.37, 1.04, 1.01

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -68,13 +68,13 @@ void hello_fun(void *arg) {
 }
 
 void init_proc() {
-  char *const argv[] = { "printenv", NULL };
-  char *const envp[] = { "PATH=/bin", NULL };
+  char *const argv[] = { "cat", "/share/files/num", NULL };
+  char *const envp[] = { NULL };
 
   Log("Initializing processes...");
 
   context_kload(&pcb[0], hello_fun, "A");
-  context_uload(&pcb[1], "/bin/printenv", argv, envp);
+  context_uload(&pcb[1], "/bin/cat", argv, envp);
   switch_boot_pcb();
 }
 
