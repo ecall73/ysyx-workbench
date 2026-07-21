60d70a9c7f2644aaf21e41ca874085869cf9e2ba
tracer-ysyx <tracer@ysyx.org>
2026-07-21 18:45:12 +0800

    > compile NEMU ysyx_26030082 李智恒 Linux ecall73-laptop 6.18.33.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun 18 21:54:43 UTC 2026 x86_64 x86_64 x86_64 GNU/Linux  18:45:12 up  3:07,  1 user,  load average: 0.05, 0.13, 0.17

--- nemu/src/cpu/difftest/dut.c
+++ nemu/src/cpu/difftest/dut.c
@@ -30,10 +30,13 @@ void (*ref_difftest_raise_intr)(uint64_t NO) = NULL;
 
 static bool is_skip_ref = false;
 static int skip_dut_nr_inst = 0;
+static bool is_attached = true;
 
 // this is used to let ref skip instructions which
 // can not produce consistent behavior with NEMU
 void difftest_skip_ref() {
+  if (!is_attached) return;
+
   is_skip_ref = true;
   // If such an instruction is one of the instruction packing in QEMU
   // (see below), we end the process of catching up with QEMU's pc to
@@ -52,6 +55,8 @@ void difftest_skip_ref() {
 //   Let REF run `nr_ref` instructions first.
 //   We expect that DUT will catch up with REF within `nr_dut` instructions.
 void difftest_skip_dut(int nr_ref, int nr_dut) {
+  if (!is_attached) return;
+
   Assert(nr_ref >= 0 && nr_dut >= 0,
       "bad difftest_skip_dut arguments: nr_ref=%d nr_dut=%d skip_dut_nr_inst=%d",
       nr_ref, nr_dut, skip_dut_nr_inst);
@@ -60,6 +65,19 @@ void difftest_skip_dut(int nr_ref, int nr_dut) {
   while (nr_ref -- > 0) {
     ref_difftest_exec(1);
   }
+}
+
+void difftest_detach() {
+  is_attached = false;
+}
+
+void difftest_attach() {
+  ref_difftest_memcpy(PMEM_LEFT, guest_to_host(PMEM_LEFT), CONFIG_MSIZE, DIFFTEST_TO_REF);
+  ref_difftest_regcpy(&cpu, DIFFTEST_TO_REF);
+  isa_difftest_attach();
+  is_skip_ref = false;
+  skip_dut_nr_inst = 0;
+  is_attached = true;
 }
 
 void init_difftest(char *ref_so_file, long img_size, int port) {
@@ -111,6 +129,8 @@ static void checkregs(CPU_state *ref, vaddr_t pc) {
 }
 
 void difftest_step(vaddr_t pc, vaddr_t npc) {
+  if (!is_attached) return;
+
   CPU_state ref_r;
 
   if (skip_dut_nr_inst > 0) {
