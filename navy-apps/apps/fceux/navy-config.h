d40bf577b48f2d30352de301615d421a260779cd
tracer-ysyx <tracer@ysyx.org>
2026-07-21 18:10:42 +0800

    > compile NEMU ysyx_26030082 李智恒 Linux ecall73-laptop 6.18.33.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun 18 21:54:43 UTC 2026 x86_64 x86_64 x86_64 GNU/Linux  18:10:42 up  2:35,  1 user,  load average: 0.40, 0.23, 0.13

---
+++ navy-apps/apps/fceux/navy-config.h
@@ -0,0 +1,12 @@
+#ifndef __FCEUX_NAVY_CONFIG_H__
+#define __FCEUX_NAVY_CONFIG_H__
+
+#define __PLATFORM_NEMU
+#include <config.h>
+
+#undef SOUND_CONFIG
+#define SOUND_CONFIG SOUND_NONE
+
+#undef __NO_FILE_SYSTEM__
+
+#endif
