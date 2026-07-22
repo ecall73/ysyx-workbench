d846950c6ce4d053ace8c4e5c3fc759e15cd65c0
tracer-ysyx <tracer@ysyx.org>
2026-07-23 03:17:51 +0800

    > compile NEMU ysyx_26030082 李智恒 Linux ecall73-laptop 6.18.33.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun 18 21:54:43 UTC 2026 x86_64 x86_64 x86_64 GNU/Linux  03:17:51 up  9:36,  1 user,  load average: 0.29, 0.22, 0.17

--- nanos-lite/include/proc.h
+++ nanos-lite/include/proc.h
@@ -20,7 +20,6 @@ extern PCB *current;
 
 uintptr_t loader(PCB *pcb, const char *filename);
 void naive_uload(PCB *pcb, const char *filename);
-void context_kload(PCB *pcb, void (*entry)(void *), void *arg);
 void context_uload(PCB *pcb, const char *filename, char *const argv[], char *const envp[]);
 void switch_boot_pcb(void);
 Context *schedule(Context *prev);
