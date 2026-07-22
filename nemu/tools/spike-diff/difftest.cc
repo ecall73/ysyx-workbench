740cdcf2b80d416e6bf15c94c9d180a12bf29aab
tracer-ysyx <tracer@ysyx.org>
2026-07-23 02:49:01 +0800

    > compile NEMU ysyx_26030082 李智恒 Linux ecall73-laptop 6.18.33.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun 18 21:54:43 UTC 2026 x86_64 x86_64 x86_64 GNU/Linux  02:49:01 up  9:08,  1 user,  load average: 0.05, 0.13, 0.15

--- nemu/tools/spike-diff/difftest.cc
+++ nemu/tools/spike-diff/difftest.cc
@@ -119,8 +119,9 @@ __EXPORT void difftest_regcpy(void* dut, bool direction) {
 
 __EXPORT void difftest_exec(uint64_t n) {
   if (state->pc >= 0x80001930 && state->pc <= 0x8000193c) {
-    fprintf(stderr, "ref before pc=%08x sp=%08x mscratch=%08x mstatus=%08x priv=%u\\n",
-        (uint32_t)state->pc, (uint32_t)state->XPR[2],
+    fprintf(stderr, "ref before pc=%08x inst=%08x sp=%08x mscratch=%08x mstatus=%08x priv=%u\\n",
+        (uint32_t)state->pc, (uint32_t)p->get_mmu()->load_insn(state->pc).insn.bits(),
+        (uint32_t)state->XPR[2],
         (uint32_t)state->csrmap[CSR_MSCRATCH]->read(),
         (uint32_t)state->mstatus->read(), (uint32_t)state->prv);
   }
