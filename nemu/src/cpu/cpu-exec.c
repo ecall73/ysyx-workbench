4d49a2e665d484260529ec3449b4aa9052ad9b36
tracer-ysyx <tracer@ysyx.org>
2026-07-23 19:50:01 +0800

    > compile NEMU ysyx_26030082 李智恒 Linux ecall73-laptop 6.18.33.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun 18 21:54:43 UTC 2026 x86_64 x86_64 x86_64 GNU/Linux  19:50:01 up  1:28,  1 user,  load average: 0.19, 0.17, 0.11

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -89,6 +89,11 @@ static void execute(uint64_t n) {
     word_t intr = isa_query_intr();
     if (intr != INTR_EMPTY) {
       cpu.pc = isa_raise_intr(intr, cpu.pc);
+#ifdef CONFIG_DIFFTEST
+      if (difftest_is_attached()) {
+        ref_difftest_raise_intr(intr);
+      }
+#endif
     }
   }
 }
