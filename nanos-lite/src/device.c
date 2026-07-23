d353255ddde7a91291581b14010a78674c766bca
tracer-ysyx <tracer@ysyx.org>
2026-07-24 01:11:43 +0800

    > compile NEMU ysyx_26030082 李智恒 Linux ecall73-laptop 6.18.33.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun 18 21:54:43 UTC 2026 x86_64 x86_64 x86_64 GNU/Linux  01:11:43 up  6:26,  1 user,  load average: 0.27, 0.09, 0.03

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -1,5 +1,11 @@
 #include <common.h>
 
+#if defined(MULTIPROGRAM) && !defined(TIME_SHARING)
+# define MULTIPROGRAM_YIELD() yield()
+#else
+# define MULTIPROGRAM_YIELD()
+#endif
+
 #define NAME(key) \
   [AM_KEY_##key] = #key,
 
@@ -9,6 +15,7 @@ static const char *keyname[256] __attribute__((used)) = {
 };
 
 size_t serial_write(const void *buf, size_t offset, size_t len) {
+  MULTIPROGRAM_YIELD();
   for (size_t i = 0; i < len; i ++) {
     putch(((const char *)buf)[i]);
   }
@@ -16,6 +23,7 @@ size_t serial_write(const void *buf, size_t offset, size_t len) {
 }
 
 size_t events_read(void *buf, size_t offset, size_t len) {
+  MULTIPROGRAM_YIELD();
   AM_INPUT_KEYBRD_T event = io_read(AM_INPUT_KEYBRD);
   if (event.keycode == AM_KEY_NONE) {
     return 0;
@@ -31,6 +39,7 @@ size_t dispinfo_read(void *buf, size_t offset, size_t len) {
 }
 
 size_t fb_write(const void *buf, size_t offset, size_t len) {
+  MULTIPROGRAM_YIELD();
   AM_GPU_CONFIG_T cfg = io_read(AM_GPU_CONFIG);
   assert(offset % sizeof(uint32_t) == 0 && len % sizeof(uint32_t) == 0);
   size_t pixel_offset = offset / sizeof(uint32_t);
