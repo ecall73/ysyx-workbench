5060ee656b89c76dd4985a8fccb7a18fb6adfc3e
ecall73 <openweskl@gmail.com>
2026-07-29 22:48:26 +0800

    fix(am): Use empty header file stubs-ilp32.h to fix the dependency issues of riscv64-linux-gnu-gcc

---
+++ abstract-machine/am/include/gnu/stubs-ilp32.h
@@ -0,0 +1,6 @@
+#ifndef __AM_GNU_STUBS_ILP32_H__
+#define __AM_GNU_STUBS_ILP32_H__
+
+// AbstractMachine does not link against the target glibc.
+
+#endif
---
+++ navy-apps/include/gnu/stubs-ilp32.h
@@ -0,0 +1,6 @@
+#ifndef __NAVY_GNU_STUBS_ILP32_H__
+#define __NAVY_GNU_STUBS_ILP32_H__
+
+/* RISC-V ILP32 Navy targets are freestanding and do not link glibc. */
+
+#endif
