5b183ab4bb720763b794d39b7ce0f83be72bec14
ecall73 <openweskl@gmail.com>
2026-08-10 23:15:47 +0800

    fix(rt-thread-am): fix rt_hw_interrupt api

--- rt-thread-am/bsp/abstract-machine/src/interrupt.c
+++ rt-thread-am/bsp/abstract-machine/src/interrupt.c
@@ -1,10 +1,13 @@
 #include <am.h>
 #include <klib.h>
+#include <rthw.h>
 
-void rt_hw_interrupt_enable() {
-  iset(1);
+void rt_hw_interrupt_enable(rt_base_t level) {
+  iset(level != 0);
 }
 
-void rt_hw_interrupt_disable() {
+rt_base_t rt_hw_interrupt_disable(void) {
+  rt_base_t level = ienabled();
   iset(0);
+  return level;
 }
